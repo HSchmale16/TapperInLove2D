b6896e65bc2c69550f6617e866a6ca7c7075c109
Henry J Schmale <hschmale16@gmail.com>
2023-04-09 17:24:06 -0400

    do more things

--- main.lua
+++ main.lua
@@ -23,9 +23,15 @@ COLOR_MUG = {250 / 255, 250 / 255, 210 / 255}
 COLOR_BEER = {139 / 255, 69 / 255, 19 / 255}
 
 COLOR_PATRON = {0, 1, 0}
+COLOR_LEAVING_PATRON = {1, 1, 0}
 
-MUG_HEIGHT = 30
-MUG_WIDTH = 15
+MUG_HEIGHT = 25
+MUG_WIDTH = 12
+
+PATRON_WIDTH = 24
+
+SCORE = 0
+
 
 -- ------------------------------------------------------
 -- Begin the lane definitions
@@ -40,7 +46,8 @@ function Lane:new (o)
         barkeep_here = false,
         mugs = {},
         TABLE_END = 900,
-        patrons = {}
+        patrons = {},
+        leaving_patrons = {}
     }
 
     for k, v in pairs(o) do
@@ -60,6 +67,11 @@ function Lane:draw()
         love.graphics.rectangle("fill", v.x, self.y - 15 - v.y, 15, 30)
     end
 
+    love.graphics.setColor(COLOR_LEAVING_PATRON)
+    for k, v in ipairs(self.leaving_patrons) do
+        love.graphics.rectangle("fill", v.x, self.y - 15 - v.y, 15, 30)
+    end
+
     -- draw the bar table
     love.graphics.setColor(COLOR_TABLE)
     love.graphics.rectangle("fill", 0, self.y, self.TABLE_END, 80)
@@ -75,12 +87,12 @@ function Lane:draw()
 
         if MUGFILL_IN_PROGRESS or MUGFILL_PERCENT > 0 then  
             local y = self.y + 2
-            local height = (MUGFILL_PERCENT / 100) * 30
-            local y2 = y + (30 - height)
+            local height = (MUGFILL_PERCENT / 100) * MUG_HEIGHT
+            local y2 = y + (MUG_HEIGHT - height)
 
             -- mug fill
             love.graphics.setColor(COLOR_BEER)
-            love.graphics.rectangle("fill", 965, y2, 15, height)
+            love.graphics.rectangle("fill", 965, y2, MUG_WIDTH, height)
 
             -- mug outline
             love.graphics.setColor(COLOR_MUG)
@@ -103,7 +115,7 @@ end
 
 
 function Lane:updateMugs(dt) 
-    local distance = 20 * dt
+    local distance = 40 * dt
     for k, v in pairs(self.mugs) do
         v.x = v.x - distance
     end
@@ -111,7 +123,15 @@ end
 
 function Lane:updatePatrons(dt) 
     for k, v in ipairs(self.patrons) do
-        v.x = v.x + 30 * dt
+        v.x = v.x + v.dx * dt
+        v.y = v.y + v.dy * dt
+        if v.y > 7 or v.y < 0 then
+            v.dy = v.dy * -1
+        end
+    end
+
+    for k, v in ipairs(self.leaving_patrons) do
+        v.x = v.x + v.dx * dt
         v.y = v.y + v.dy * dt
         if v.y > 7 or v.y < 0 then
             v.dy = v.dy * -1
@@ -122,6 +142,39 @@ end
 function Lane:update(dt)
     self:updateMugs(dt)
     self:updatePatrons(dt)
+
+    -- we only have to test the first mug and first patron to see if they have their beer
+    -- if they have any beers remaining they get shoved back.
+    -- the score also goes up for matching them.
+    -- maybe should update to searching for patrons on a given beer.
+    -- Max number of steps will be 5.
+    if #self.patrons > 0 and #self.mugs > 0 then
+        local patron = self.patrons[1]
+        local beer = self.mugs[1]
+        if patron.x > beer.x and beer.x < (patron.x + PATRON_WIDTH) then 
+            -- the mug is consumed
+            table.remove(self.mugs, 1)
+            SCORE = SCORE + 100
+
+            patron.beers_required = patron.beers_required - 1
+            if patron.beers_required <= 0 then
+                -- people who need 0 more beers should be told to go away
+                patron.dx = -50
+
+                -- move to leaving_patrons because we don't want to collide with losers
+                table.remove(self.patrons, 1)
+                table.insert(self.leaving_patrons, patron)
+            else
+                -- TODO: Cover drinking a beer animation
+            end
+        end
+    end
+
+    if #self.patrons > 0 then
+    end
+
+    if #self.mugs > 0 then 
+    end
 end
 
 function Lane:sendMug()
@@ -133,10 +186,11 @@ end
 function Lane:spawnPatron()
     table.insert(self.patrons, {
         x  = 0,
+        dx = 20,
         y  = love.math.random(0, 4),
         dy = love.math.random(6, 8),
         sprite = 0,
-        beers_required = love.math.random(2)
+        beers_required = 1
     })
 end
 
@@ -145,12 +199,6 @@ end
 -- BEGIN GAME LOGIC
 -- ------------------------------------------------------
 
-lanes = {
-    Lane:new{y=200, barkeep_here=true},
-    Lane:new{y=350},
-    Lane:new{y=500},
-    Lane:new{y=650}
-}
 
 -- where the players is. Which lane
 BARKEEP_INDEX = 1
@@ -166,6 +214,7 @@ PATRON_SPAWNED_LAST = 1
 PATRON_COUNTER = 0
 
 function love.load() 
+    resetGame()
 end
 
 function love.draw() 
@@ -173,7 +222,7 @@ function love.draw()
         v:draw()
     end
 
-    txt = string.format("score = %d fps = %d", 0, love.timer.getFPS())
+    txt = string.format("score = %d fps = %d", SCORE, love.timer.getFPS())
     love.graphics.print(txt, 10, 10)
 end
 
@@ -200,8 +249,9 @@ end
 
 function love.keypressed(key, scancode, isrepeat) 
     if key == 'a' then
-        finishMugFill()
         -- move barkeep down
+        MUGFILL_IN_PROGRESS = false
+        MUGFILL_PERCENT = 0
         lanes[BARKEEP_INDEX].barkeep_here = false
         BARKEEP_INDEX = (BARKEEP_INDEX + 1) % (#lanes + 1)
         if BARKEEP_INDEX == 0 then
@@ -211,7 +261,8 @@ function love.keypressed(key, scancode, isrepeat)
 
     elseif key == 'd' then
         -- move barkeep up
-        finishMugFill()
+        MUGFILL_IN_PROGRESS = false
+        MUGFILL_PERCENT = 0
         lanes[BARKEEP_INDEX].barkeep_here = false
         BARKEEP_INDEX = (BARKEEP_INDEX - 1) % (#lanes + 1)
         if BARKEEP_INDEX == 0 then
@@ -241,5 +292,17 @@ function finishMugFill()
         item:sendMug()
     end
     MUGFILL_PERCENT = 0
+    MUGFILL_IN_PROGRESS = false
+end
+
+function resetGame() 
+    SCORE = 0
+    lanes = {
+        Lane:new{y=200, barkeep_here=true},
+        Lane:new{y=350},
+        Lane:new{y=500},
+        Lane:new{y=650}
+    }
+    MUGFILL_IN_PROGRESS = 0
     MUGFILL_IN_PROGRESS = false
 end
